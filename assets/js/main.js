7549b46b2e02602825e0fc97243806eb201dc4af
HosseinEnsafi <HosseinEnsafi@outlook.com>
2022-09-08 13:04:43 +0430

    init and setup project

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// ================= VARIABLES =================
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+// ================= SHOW SHOW =================
+
+// ================= HIDE MENU =================
+
+// ================= REMOVE MOBILE MENU =================
