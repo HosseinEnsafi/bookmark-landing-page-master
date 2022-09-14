ae81ee126ecc604babbde7f4f2b90015583613fa
HosseinEnsafi <HosseinEnsafi@outlook.com>
2022-09-14 12:57:14 +0430

    create header menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,22 @@
 // ================= VARIABLES =================
 const navMenu = document.getElementById("nav-menu"),
   navToggle = document.getElementById("nav-toggle"),
-  navClose = document.getElementById("nav-close");
+  navClose = document.getElementById("nav-close"),
+  header = document.querySelector("header.header"),
+  navLinks = document.querySelectorAll("header .nav__item");
 
 // ================= SHOW SHOW =================
+navToggle.addEventListener("click", (e) => {
+  header.classList.add("mobile-menu--active");
+});
 
 // ================= HIDE MENU =================
-
+navClose.addEventListener("click", () => {
+  header.classList.remove("mobile-menu--active");
+});
 // ================= REMOVE MOBILE MENU =================
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", () => {
+    header.classList.remove("mobile-menu--active");
+  });
+});
