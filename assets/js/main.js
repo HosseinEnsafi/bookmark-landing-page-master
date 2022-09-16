011e8aac55e618767f38a4156e3fe0a2b38194f4
HosseinEnsafi <HosseinEnsafi@outlook.com>
2022-09-16 14:20:47 +0430

    add features section logic and markup

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,10 @@ const navMenu = document.getElementById("nav-menu"),
   navToggle = document.getElementById("nav-toggle"),
   navClose = document.getElementById("nav-close"),
   header = document.querySelector("header.header"),
-  navLinks = document.querySelectorAll("header .nav__item");
+  navLinks = document.querySelectorAll("header .nav__item"),
+  tabs = document.querySelectorAll(".features__tab"),
+  tabsContainer = document.querySelector(".features__tabs"),
+  tabsContent = document.querySelectorAll(".features__content");
 
 // ================= SHOW SHOW =================
 navToggle.addEventListener("click", (e) => {
@@ -21,4 +24,20 @@ navLinks.forEach((navLink) => {
   navLink.addEventListener("click", () => {
     header.classList.remove("mobile-menu--active");
   });
+});
+
+// =================  FEATURES SECTION  TABS  =================
+tabsContainer.addEventListener("click", (e) => {
+  const tabClicked = e.target.classList.contains("features__tab");
+  if (!tabClicked) return;
+
+  tabs.forEach((tab) => tab.classList.remove("features__tab--active"));
+  tabsContent.forEach((tabC) =>
+    tabC.classList.remove("features__content--active")
+  );
+  const tabId = e.target.attributes["data-tab"].value;
+
+  document
+    .querySelector(`.features__content--${tabId}`)
+    .classList.add("features__content--active");
 });
